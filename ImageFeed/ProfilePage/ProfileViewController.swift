9ea4d5298d87650dfed098df9f9fba0d2383c033
Chivos <chemodanov.iv@gmail.com>
2023-06-10 10:27:49 +0300

    init branch sprint_09

---
+++ ImageFeed/ProfilePage/ProfileViewController.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet private var avatarView: UIImageView!
+    @IBOutlet private var logoutButton: UIButton!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
